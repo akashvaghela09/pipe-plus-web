54b7533a1a7f33c06a89506279bfd78611dfe7ee
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-04-23 03:37:27 +0530

    Add index file for importing

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// Player components
+export { Player } from "./player/Player.jsx";
+export { Controls } from "./player/Controls.jsx";
+
+// Theme components
+export { IconWrapper } from "./theme/IconWrapper.jsx"
