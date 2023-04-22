72e8a8709433cd90961f7b81c99d6af242d0c79f
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-04-23 03:38:18 +0530

    Update app for testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { pipePlus } from './apis/pipePlus';
+import { Player } from './components';
 
 function App() {
+  const [playList, setPlayList] = useState([]); // [ { url: "", track: "", mimeType: "", quality: "", videoOnly: "", height: "", width: "" }
+
+  const getStreamData = async () => {
+    let data = await pipePlus.getStreamData("ElZfdU54Cp8")
+
+    console.log(data);
+    // setPlayList(data.resourceList);
+  }
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      REACT
-    </h1>
+    <div className="App bg-slate-500">
+      <button onClick={() => getStreamData()} className='p-2 m-8 bg-blue-500 rounded-md text-slate-100 w-36'>
+        Fetch
+      </button>
+
+      <Player />
     </div>
   );
 }
