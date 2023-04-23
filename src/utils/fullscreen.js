a2b0ca55ffa21ea5f621b7f7a27f0f0dea2ffd81
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-04-23 16:35:27 +0530

    Add fullscreen utils

---
+++ src/utils/fullscreen.js
@@ -0,0 +1,23 @@
+export const requestFullScreenEnter = () => {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+    }
+}
+
+export const requestFullScreenExit = () => {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+};
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { requestFullScreenEnter, requestFullScreenExit } from './fullscreen';
