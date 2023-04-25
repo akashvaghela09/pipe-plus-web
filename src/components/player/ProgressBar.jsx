ccbc427b187c7208710f85bab72c8eeb18054f9f
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-04-26 00:02:11 +0530

    Update progress bar

--- src/components/player/ProgressBar.jsx
+++ src/components/player/ProgressBar.jsx
@@ -1,29 +1,12 @@
-import React, { useEffect, useState } from 'react';
-
-export const ProgressBar = ({amountLoaded, amountPlayed}) => {
-
-    const [progress, setProgress] = useState(0);
-    const [bufferWidth, setBufferWidth] = useState(amountLoaded);
-
-    
-    const handleCahnge = (value) => {
-        setProgress(value);
-    }
-    
-    useEffect(() => {
-    }, [amountPlayed, progress]);
-
-    useEffect(() => {
-        setBufferWidth(amountLoaded);
-    }, [amountLoaded]);
+export const ProgressBar = ({progress, amountLoaded, amountPlayed, handleSeek}) => {
 
     return (
-        <div className='absolute bottom-14 w-full flex flex-col px-4'>
+        <div className='absolute bottom-14 flex flex-col px-4 mx-5' style={{width: `calc(100% - 40px)`}}>
             <div className='absolute top-0 left-0 bg-zinc-800 h-1 mt-1'
                 style={{ width: `${100}%` }}
             />
             <div className={`absolute top-0 left-0 w-1/2 bg-zinc-600 h-1 mt-1`}
-                style={{ width: `${bufferWidth}%` }}
+                style={{ width: `${amountLoaded}%` }}
             />
             <div
                 className="absolute top-0 left-0 bg-red-600 h-1 mt-1"
@@ -35,8 +18,8 @@ export const ProgressBar = ({amountLoaded, amountPlayed}) => {
                 max={100}
                 step={0.001}
                 value={progress}
-                onChange={(e) => handleCahnge(e.target.value)}
-                className='pp_input absolute top-0 left-0 appearance-none w-full h-4 pb-1 bg-transparent cursor-pointer'
+                onChange={(e) => handleSeek(e.target.value)}
+                className='pp_input absolute top-0 left-0 appearance-none w-full h-4 pb-1 bg-transparent cursor-pointer z-50'
             />
             <div />
         </div>
