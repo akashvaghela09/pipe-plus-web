f7bf637926d1e74b4f05b14bc3abc0fdce68cd5c
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-04-30 23:54:29 +0530

    Add homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { pipePlus } from "../apis"
+import { VideoCard } from "../components"
 export const Home = () => {
+    const dispatch = useDispatch()
+    const [trendingStreams, setTrendingStreams] = useState([]);
+
+    const fetchTrendingStreams = async () => {
+
+        try {
+            let res = await pipePlus.getTrendingData("IN");
+            setTrendingStreams(res);
+        } catch (error) {
+            console.log("Something went wrong");
+        }
+    }
+
+    useEffect(() => {
+        fetchTrendingStreams()
+    }, [])
+
     return (
-        <div>
-            <h1 className="text-3xl text-slate-100 font-bold">Home</h1>
+        <div className="h-full flex flex-wrap justify-start gap-4">
+            {
+                trendingStreams.length > 0 && trendingStreams.map((item) => {
+                    return <VideoCard key={item.title} video={item} />
+                })
+
+            }
         </div>
     )
 }
