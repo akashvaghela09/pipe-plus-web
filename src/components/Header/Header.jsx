c76aaf0347669571202161d0871e11bdd4daddd8
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-04-30 23:55:20 +0530

    Fix searchbar zindex issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { FiMenu } from 'react-icons/fi';
 
 export const Header = () => {
     return (
-        <div className='flex items-center justify-between h-16 w-full pl-2'>
+        <div className='sticky top-0 bg-[#0f0f0f] flex items-center justify-between h-16 w-full pl-2 z-50'>
             <div className='w-[250px] flex justify-start items-center'>
                 <div className='cursor-pointer hover:bg-[#272727] flex justify-center items-center h-10 w-10 rounded-full mx-2'>
                     <FiMenu className='text-slate-50 text-2xl' />
