8330681c803b0621ea191d1a2f5f5f4842683dc4
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-05-24 23:25:35 +0530

    Update watch page

--- src/routes/Watch.jsx
+++ src/routes/Watch.jsx
@@ -1,18 +1,33 @@
 import { useSearchParams } from "react-router-dom";
-import { Player } from "../components";
+import { Button, ButtonWrapper, DescriptionCard, Player } from "../components";
 import { useEffect } from "react";
 import { pipePlus } from "../apis/pipePlus";
 import { useDispatch, useSelector } from "react-redux";
 import { setAvailableQualities, setStreamMetadata, setStreamSource } from "../redux/player/actions";
 import { v4 as uuid } from 'uuid';
+import { formatNumbers } from "../utils";
+import { TiTick } from 'react-icons/ti';
+import { BiSolidDownload, BiLike, BiDislike } from 'react-icons/bi';
 
 export const Watch = () => {
     const dispatch = useDispatch();
     const [searchParams, setSearchParams] = useSearchParams();
     const videoId = searchParams.get('v')
 
-    const { streamMetadata, selectedQuality } = useSelector(state => state.player);
-    const { title, thumbnailUrl, description } = streamMetadata;
+    const { streamMetadata, selectedQuality, streamSource } = useSelector(state => state.player);
+    const { 
+        title, 
+        thumbnailUrl, 
+        description, 
+        uploaderAvatar, 
+        uploader, 
+        uploaderSubscriberCount, 
+        likes,
+        dislikes,
+        views,
+        uploadDate,
+        relatedStreams 
+    } = streamMetadata;
 
     const handleVideoId = async () => {
         let res = await pipePlus.getStreamData(videoId);
@@ -22,10 +37,10 @@ export const Watch = () => {
         // prepare stream resources 
         let listOfStreams = {};
 
-        for(let i = 0; i < videoStreams.length; i++) {
+        for (let i = 0; i < videoStreams.length; i++) {
             let stream = videoStreams[i];
 
-            if(listOfStreams[stream.quality]) {
+            if (listOfStreams[stream.quality]) {
                 listOfStreams[stream.quality].push(stream);
             } else {
                 listOfStreams[stream.quality] = [stream];
@@ -35,7 +50,7 @@ export const Watch = () => {
         let availableQuality = Object.keys(listOfStreams);
         let streamSource = {};
 
-        if(selectedQuality) {
+        if (selectedQuality) {
             streamSource = listOfStreams[selectedQuality][0];
         } else {
             streamSource = listOfStreams[availableQuality[0]][0];
@@ -45,7 +60,7 @@ export const Watch = () => {
 
         let qualityList = []
 
-        for(let i = 0; i < availableQuality.length; i++) {
+        for (let i = 0; i < availableQuality.length; i++) {
             let obj = {};
             obj.id = uuid();
             obj.quality = availableQuality[i];
@@ -53,31 +68,64 @@ export const Watch = () => {
             qualityList.push(obj);
         }
 
-        // console.log("selected quality: ", quality);
-        // console.log("available quality: ", availableQuality);
-        // console.log("sourceList: ", sourceList);
-        // console.log("listOfStreams: ", listOfStreams);
-
-
         dispatch(setStreamSource(streamSource));
         dispatch(setAvailableQualities(qualityList));
-        dispatch(setStreamMetadata({...res, playableStreams: listOfStreams}));
+        dispatch(setStreamMetadata({ ...res, playableStreams: listOfStreams }));
     }
+
+    
 
     useEffect(() => {
         handleVideoId()
     }, [videoId])
 
     return (
-        <div className="w-full pt-10 flex justify-center">
-            <div className="w-[60%] flex flex-col gap-4">
+        <div className="w-10/12 max-w-10/12 pt-6 flex justify-center">
+            <div className="grow flex flex-col gap-4">
                 <Player videoId={videoId} />
-                <div className="">
-                    <p className="text-slate-100 text-2xl">{title}</p>
-                    {/* <p>{description}</p> */}
+                <div className="flex flex-col gap-2">
+                    <p className="text-slate-100 text-xl font-medium font-sans">{title}</p>
+
+                    <div className="flex justify-between items-center">
+                        <div className="py-2 flex gap-3 justify-start items-center">
+                            <img src={uploaderAvatar} className="rounded-full" />
+                            <div className="flex flex-col justify-center">
+                                <div className="flex items-center gap-2">
+                                    <p className="text-slate-200 text-sm">{uploader} </p>
+                                    <TiTick className="bg-slate-50 bg-opacity-50 rounded-full text-xs" />
+                                </div>
+                                <p className="text-slate-200 text-sm">{formatNumbers(uploaderSubscriberCount)} subscribers</p>
+                            </div>
+                            <Button className="mx-5">
+                                Subscribe
+                            </Button>
+                        </div>
+                        <div className="flex gap-4">
+                            <Button>
+                                <BiLike className="text-xl" />
+                                {formatNumbers(likes)}
+                            </Button>
+                            <Button>
+                                <BiDislike className="text-xl" />
+                                {formatNumbers(dislikes)}
+                            </Button>
+                            <Button>
+                                <BiSolidDownload className="text-xl" />
+                                Download
+                            </Button>
+                        </div>
+                    </div>
+
+                    <div className="w-full">
+                        <DescriptionCard
+                            views={views}
+                            uploadDate={uploadDate}
+                            description={description}
+                        />
+                    </div>
                 </div>
             </div>
-            <div className="w-[400px]">
+            <div className="w-[450px] min-w-[450px]">
 
             </div>
         </div>
