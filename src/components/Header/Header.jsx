b839f56ae43ab25fad6d7712e5daa0fc4d01cdba
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-05-24 23:22:48 +0530

    Add home redirect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Logo, SearchBar } from '../';
 import { FiMenu } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSidePanelValue } from "../../redux/app/actions";
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
     const dispatch = useDispatch();
@@ -17,9 +18,11 @@ export const Header = () => {
         <div className='sticky top-0 bg-[#0f0f0f] flex items-center justify-between h-16 w-full pl-2 z-50'>
             <div className='w-[250px] flex justify-start items-center'>
                 <div className='cursor-pointer hover:bg-[#272727] flex justify-center items-center h-10 w-10 rounded-full mx-2'>
-                    <FiMenu className='text-slate-50 text-2xl' onClick={() => toggleSidePanel()}/>
+                    <FiMenu className='text-slate-50 text-2xl' onClick={toggleSidePanel}/>
                 </div>
-                <Logo />
+                <Link to={"/"}>
+                    <Logo />
+                </Link>
             </div>
             <SearchBar />
             <div className='w-[250px]' />
--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,6 +1,6 @@
-export const Logo = () => {
+export const Logo = ({onClick}) => {
     return (
-        <div className='flex justify-center items-center p-2 w-fit h-fit cursor-pointer select-none'>
+        <div onClick={onClick} className='flex justify-center items-center p-2 w-fit h-fit cursor-pointer select-none'>
             <p className='text-red-600 text-2xl font-extrabold font_oswald'>Pipe</p>
             <p className='text-slate-50 text-2xl font-extrabold font_oswald'>Plus</p>
         </div>
