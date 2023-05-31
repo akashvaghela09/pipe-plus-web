63f45154144f828dfd0460b0ed6eb6b254fc89ae
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-05-31 23:56:50 +0530

    Add signout button

--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -1,7 +1,27 @@
+import { setAuthStatus, setUser } from "../redux/auth/actions";
+import { signOutUser } from "../utils";
+import { useDispatch,  } from "react-redux";
+
 export const Settings = () => {
+    const dispatch = useDispatch();
+
+    const handleSignOut = async () => {
+        try {
+            await signOutUser();
+
+            dispatch(setAuthStatus(false));
+            dispatch(setUser({}));
+        } catch (error) {
+            alert("Error signing out");
+            console.log(error);
+        }
+    }
+    
     return (
         <div>
-            <h1 className="text-3xl text-slate-100 font-bold">Settings</h1>
+            <button onClick={handleSignOut} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Sing out
+            </button>
         </div>
     )
 }
