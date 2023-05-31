d5ceee911bc3cd514a44226234ac4c827d5c206b
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-05-31 23:57:26 +0530

    Fix react key issue

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,16 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { pipePlus } from "../apis";
 import { VideoCard } from "../components";
 import { Link } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
 export const Home = () => {
     const dispatch = useDispatch()
     const { sidepanelOpen } = useSelector((state) => state.app);
+    const { authStatus } = useSelector((state) => state.auth);
     const [trendingStreams, setTrendingStreams] = useState([]);
 
     const fetchTrendingStreams = async () => {
-
         try {
             let res = await pipePlus.getTrendingData("IN");
+            res.forEach((item) => {
+                item.id = uuid();
+            });
+
             setTrendingStreams(res);
         } catch (error) {
             console.log("Something went wrong");
@@ -27,7 +32,7 @@ export const Home = () => {
         <div className="h-full flex flex-wrap justify-start gap-4 p-10" style={{marginLeft: sidepanelOpen ? "270px": "0px"}}>
             {
                 trendingStreams.length > 0 && trendingStreams.map((item) => {
-                    return <Link to={item.url}>
+                    return <Link to={item.url} key={item.id}>
                     <VideoCard key={item.title} video={item} />
                     </Link>
                 })
