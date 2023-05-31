e6b6142bf95f0262aa863b14897b1a909de7bbdb
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-05-31 23:53:46 +0530

    Check user auth status

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Header, Sidepanel } from './components';
 import { AllRoutes } from './routes';
+import { useDispatch } from 'react-redux';
+import { getUser, getSession } from './utils';
+import { setAuthStatus, setUser } from './redux/auth/actions';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const checkAuthStatus = async () => {
+
+    const session = await getSession();
+    if (session !== null) {
+      const { aud, id, email } = await getUser();
+
+      if (aud === "authenticated") {
+        dispatch(setAuthStatus(true));
+        dispatch(setUser({ id: id, email: email }));
+      } else {
+        dispatch(setAuthStatus(false));
+        console.log("User is not authenticated");
+      }
+    } else {
+      dispatch(setAuthStatus(false));
+      console.log("User is not authenticated");
+    }
+  }
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
 
   return (
     <div className="App flex flex-col">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
-import { Logo, SearchBar } from '../';
+import { Logo, SearchBar, SignInButton, SignOutButton } from '../';
 import { FiMenu } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSidePanelValue } from "../../redux/app/actions";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Header = () => {
     const dispatch = useDispatch();
+    const currentPath = useLocation().pathname;
 
     const { sidepanelOpen } = useSelector((state) => state.app);
+    const { authStatus } = useSelector((state) => state.auth);
 
     const toggleSidePanel = () => {
         dispatch(setSidePanelValue(!sidepanelOpen));
     }
 
     return (
-        <div className='sticky top-0 bg-[#0f0f0f] flex items-center justify-between h-16 w-full pl-2 z-50'>
+        <div className='sticky top-0 bg-[#0f0f0f] flex items-center justify-between w-full p-2 z-50'>
             <div className='w-[250px] flex justify-start items-center'>
                 <div onClick={toggleSidePanel} className='cursor-pointer hover:bg-[#272727] flex justify-center items-center h-10 w-10 rounded-full mx-2'>
-                    <FiMenu className='text-slate-50 text-2xl'/>
+                    <FiMenu className='text-slate-50 text-2xl' />
                 </div>
                 <Link to={"/"}>
                     <Logo />
                 </Link>
             </div>
+
             <SearchBar />
-            <div className='w-[250px]' />
+
+            <div className='flex justify-end w-[250px] '>
+                {
+                    currentPath !== "/signin" && authStatus === false && <SignInButton />
+                }
+            </div>
         </div>
     )
 }
