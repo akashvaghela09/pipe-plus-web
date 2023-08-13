2e9d6ff392185ffca42f3966a42dd724998af063
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-13 23:56:10 +0530

    Update for mobile styles

--- src/components/theme/Button.jsx
+++ src/components/theme/Button.jsx
@@ -1,6 +1,6 @@
-export const Button = ({ children, className, onClick }) => {
+export const Button = ({ children, className, onClick, type }) => {
     return (
-        <div onClick={onClick} className={`bg-[#272727] text-slate-200 hover:bg-[#373737] flex gap-2 items-center justify-center w-fit h-fit px-4 py-1  rounded-full cursor-pointer ${className}`}>
+        <div onClick={onClick} className={`bg-[#272727] ${type === "white" ? "text-black" : "text-slate-100"} lg:hover:bg-[#373737] flex gap-2 items-center justify-center w-fit h-fit px-2 lg:px-4 py-1  rounded-full cursor-pointer ${className}`}>
             {children}
         </div>
     )
